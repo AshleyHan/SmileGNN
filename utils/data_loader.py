f4a0b08a1d1d74f2b8117103692967bf2125d73e
AshleyHan <48547406+AshleyHan@users.noreply.github.com>
2021-06-07 18:27:41 +0800

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from KGNN_smiles.utils import format_filename
-from KGNN_smiles.config import PROCESSED_DATA_DIR, TRAIN_DATA_TEMPLATE, DEV_DATA_TEMPLATE, TEST_DATA_TEMPLATE
+from SmileGNN.utils import format_filename
+from SmileGNN.config import PROCESSED_DATA_DIR, TRAIN_DATA_TEMPLATE, DEV_DATA_TEMPLATE, TEST_DATA_TEMPLATE
 
 
 def load_data(dataset: str, data_type: str):
